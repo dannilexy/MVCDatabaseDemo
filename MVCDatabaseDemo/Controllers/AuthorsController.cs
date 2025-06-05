c3afe443776c9a5f3969c721bb196dcf7c4ed475
El-Dorado <Dannilexy@gmail.com>
2025-06-05 17:21:04 +0100

    EF fetch and create data

---
+++ MVCDatabaseDemo/Controllers/AuthorsController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVCDatabaseDemo.Data;
+using MVCDatabaseDemo.Models;
+
+namespace MVCDatabaseDemo.Controllers
+{
+    public class AuthorsController(BookStoreContext _context) : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var authors = await _context.Authors.ToListAsync();
+            return View(authors);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Author author)
+        {
+
+            var authorExists = await _context.Authors.AnyAsync(x => x.Name == author.Name);
+            if (authorExists)
+            {
+                ModelState.AddModelError("Name", "Author with name already exists.");
+                ViewBag.ErrorMessage = "Author with name already exists.";
+                return View(author);
+            }
+
+            if (ModelState.IsValid)
+            {
+                await _context.Authors.AddAsync(author);
+                //await _context.AddAsync(author);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewBag.ErrorMessage = "Invalid data. Please check the form.";
+
+
+            return View();
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var authors = await _context.Authors.FirstOrDefaultAsync(x => x.Id == id);
+            return View(authors);
+        }
+    }
+}
--- MVCDatabaseDemo/Models/Author.cs
+++ MVCDatabaseDemo/Models/Author.cs
@@ -1,7 +1,10 @@
-﻿namespace MVCDatabaseDemo.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCDatabaseDemo.Models
 {
     public class Author : BaseEntity
     {
+        [Required]
         public string Name { get; set; }
         public IEnumerable<Book> Books { get; set; } = default;
     }
